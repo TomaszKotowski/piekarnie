4fe30a042d9d429a970d2823d961d26ba6847754
Tomasz Kotowski <t.kotowski.2@gmail.com>
2022-08-25 18:47:20 +0200

    Use bakeries from database

--- src/components/sections/bakeries-list.tsx
+++ src/components/sections/bakeries-list.tsx
@@ -3,10 +3,10 @@ import { trpc } from "@/utils/trpc";
 import { Card } from "../card";
 
 export function BakeriesList() {
-  const { data: bakeries } = trpc.useQuery(["notion.getAll"]);
+  const { data: bakeries } = trpc.useQuery(["bakery.getAll"]);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mx-3">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mx-3 my-3">
       {bakeries?.map(({ id, name, info, address, socialMediaLinks }) => (
         <Card
           key={id}
